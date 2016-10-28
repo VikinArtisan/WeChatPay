2b2b74f0ebca3c4ac7ef6bc6d6ed98816382d5bd
vikin <986465329@qq.com>
2016-10-28 20:10:25 +0800

    test

--- src/Native.php
+++ src/Native.php
@@ -30,7 +30,7 @@ class Native {
 		$WxPayUnifiedOrder->SetTime_start(date("YmdHis"));  //开始时间
 		$WxPayUnifiedOrder->SetTime_expire(date("YmdHis", time() + 600));   //到期时间
 		$WxPayUnifiedOrder->SetGoods_tag("标签");   //商品标签
-		$WxPayUnifiedOrder->SetNotify_url("http://kmsc.cc/weixin/notify");    //通知地址
+		$WxPayUnifiedOrder->SetNotify_url("http://www.kmsc.cc/weixin/notify");    //通知地址
 		$WxPayUnifiedOrder->SetTrade_type("NATIVE");    //支付类型(扫码支付)
 		$WxPayUnifiedOrder->SetProduct_id("123456789"); //商品id
 
