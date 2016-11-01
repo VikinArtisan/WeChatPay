76166814a222bd35416b360fc1419f3ec3ad3fc6
vikin <986465329@qq.com>
2016-11-01 15:25:33 +0800

    微信支付成功与取消

--- src/JsApi.php
+++ src/JsApi.php
@@ -3,14 +3,15 @@ namespace Vikin\WeChatPay;
 
 use Illuminate\Support\Facades\App;
 
-class JsApi {
+class JsApi
+{
 
     //打印输出数组信息
     public function printf_info($data)
     {
         $info = '';
 
-        foreach($data as $key=>$value){
+        foreach ($data as $key => $value) {
             $info .= "<font color='#00ff55;'>$key</font> : $value <br/>";
         }
 
@@ -18,23 +19,24 @@ class JsApi {
     }
 
     //①、获取用户openid
-    public function getUserOpenId ()
+    public function getUserOpenId()
     {
         $JsApiPay = App::make('Vikin\WeChatPay\Resource\JsApiPay');
         $openId = App::call([$JsApiPay, 'GetOpenid']);
 
 
         $this->GenerateOrders($openId);
+
         return $openId;
     }
 
     //②、统一下单
-    public function GenerateOrders ($openId)
+    public function GenerateOrders($openId)
     {
         $WxPayUnifiedOrder = App::make('Vikin\WeChatPay\Resource\Lib\WxPayUnifiedOrder');
         $WxPayUnifiedOrder->SetBody("test");
         $WxPayUnifiedOrder->SetAttach("test");
-        $WxPayUnifiedOrder->SetOut_trade_no(config('WeChatConfig.MCHID').date("YmdHis"));
+        $WxPayUnifiedOrder->SetOut_trade_no(config('WeChatConfig.MCHID') . date("YmdHis"));
         $WxPayUnifiedOrder->SetTotal_fee("1");
         $WxPayUnifiedOrder->SetTime_start(date("YmdHis"));
         $WxPayUnifiedOrder->SetTime_expire(date("YmdHis", time() + 600));
@@ -44,13 +46,10 @@ class JsApi {
         $WxPayUnifiedOrder->SetOpenid($openId);
 
         $WxPayApi = App::make('Vikin\WeChatPay\Resource\Lib\WxPayApi');
-        $order = App::call([$WxPayApi, 'unifiedOrder'], ['inputObj'=>$WxPayUnifiedOrder]);
-
-        echo '<font color="#f00"><b>统一下单支付单信息</b></font><br/>';
-        var_dump($order);
+        $order = App::call([$WxPayApi, 'unifiedOrder'], ['inputObj' => $WxPayUnifiedOrder]);
 
         $JsApiPay = App::make('Vikin\WeChatPay\Resource\JsApiPay');
-        $jsApiParameters = App::call([$JsApiPay, 'GetJsApiParameters'], ['UnifiedOrderResult'=>$order]);
+        $jsApiParameters = App::call([$JsApiPay, 'GetJsApiParameters'], ['UnifiedOrderResult' => $order]);
 
 
         //获取共享收货地址js函数参数
@@ -64,10 +63,16 @@ class JsApi {
                              $jsApiParameters,
                             function(res){
                                 WeixinJSBridge.log(res.err_msg);
-                                alert(res.err_code+res.err_desc+res.err_msg);
-                                alert(res.err_code);
-                                alert(res.err_desc);
-                                alert(res.err_msg)
+                                
+                                //res.err_code    支付成功:undefined                        //取消支付:undifined
+                                //res.err_desc    支付成功:undefined                        //取消支付:undefined
+                                //res.err_msg     支付成功:get_brand_wcpay_request:ok       //取消支付:get_brand_wcpay_request:cancel
+                                
+                                if(res.err_msg === 'get_brand_wcpay_request:ok'){
+                                    self.location='/wx/pay/success';
+                                }else if(res.err_msg === 'get_brand_wcpay_request:cancel'){
+                                    self.location='/wx/pay/cancel';
+                                }
                             }
                         );
                     }
